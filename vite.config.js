1586adb2bafaa7873a70cb83a2d5f8cfa4049e42
Akpoteheri Omeru <kpomeru@gmail.com>
2022-10-11 19:57:14 +0100

    minor: Vite update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            input: [
+                "resources/sass/app.scss",
+                "resources/sass/trix-custom.scss",
+                "resources/js/app.js",
+            ],
             refresh: true,
             valetTls: "tallblog.test",
         }),
